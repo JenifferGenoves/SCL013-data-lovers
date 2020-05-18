6f05bacf17415bab728133869180101244a40d3e
Jeniffer Katherine Genoves Estrada <Jenita@MacBook-Air-de-Jeniffer.local>
2020-05-18 09:08:28 -0400

    cambios css

--- src/main.js
+++ src/main.js
@@ -6,30 +6,33 @@ import {} from './data.js';
 
 
 //Ver los Pokemones
-// Objet.values Devuelve un array con los valores correspondientes a las propiedades enumerables de la data
+// Objet.values devuelve un arreglo con los objetos pokemon (cada pokemon)
 const pokeDatos = pokeData.pokemon;
 let arrData = Object.values(pokeDatos)
-const pokemonesObj = document.getElementById("containerPokemon");
+const pokemonesObj = document.getElementById('containerPokemon');
 for (let i = 0; i < arrData.length; i++) {
-  pokemonesObj.innerHTML += `<div class="cardP">
-    <h3 class="pokeNumber">${arrData[i].num}</h3>
-    <img class="pokeImg" src="${arrData[i].img}">
-    <h4 class="pokeName">${arrData[i].name}</h4>
-  </div>`;
+  pokemonesObj.innerHTML +=
+    '<div class=\'cardP\'> <h3 class=\'pokeNumber\'>' +
+    arrData[i].num + '</h3> <img src=\'' +
+    arrData[i].img + '\' class=\'pokeImg\'> <h4 class=\'pokeName\'>' +
+    arrData[i].name + '</h4> </div>';
 }
-
 
 //Top 10
 const pokeDatos2 = pokeData.pokemon;
 let arrData2 = Object.values(pokeDatos2)
-const pokemonesObj2 = document.getElementById("tableOne");
-for (let i = 0; i <=9; i++) {
-   pokemonesObj2.innerHTML += `<div class="root">
-     <h3 class="pokeNumber2">${arrData2[i].num}</h3>
-     <h4 class="pokeName2">${arrData2[i].name}</h4>
-      <img class="pokeImg2" src="${arrData2[i].img}">
-    </div>`;
+const pokemonesObj2 = document.getElementById('tableOne');
+for (let i = 0; i <= 9; i++) {
+  pokemonesObj2.innerHTML +=
+    '<div class=\'root\'> <h3 class=\'pokeNumber2\'>' +
+    arrData2[i].num + '</h3> <h4 class=\'pokeName2\'>' +
+    arrData2[i].name + '</h4> <img src=\'' +
+    arrData2[i].img + '\' class=\'pokeImg2\'> </div>';
 }
+import pokeData from './data/pokemon/pokemon.js';
+import {} from './data.js';
+
+
 
 
 
